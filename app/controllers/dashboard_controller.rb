8e806999a4a15bc24005258fbbe81c547e16dfd3
gabem <gabemcmillan@gmail.com>
2012-08-18 17:25:14 -0500

    removed sphinx stuff from dashboard_controller, not sure how it got there

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,10 +3,7 @@ class DashboardController < ApplicationController
   
   
   
-  bin_path: /usr/bin/sphinx-searchd
-  searchd_binary_name: sphinx-searchd
-  indexer_binary_name: sphinx-indexer
-  
+
   
   def index
   end
