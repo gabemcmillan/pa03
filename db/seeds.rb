a8a7a80be42da25d7b2c0475097aa31b3a90e242
gabem <gabemcmillan@gmail.com>
2012-09-01 23:25:06 -0500

    added categories seed content

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,79 @@ Product.create(:name => 'Advice1', :price => 10)
 Product.create(:name => 'Advice2', :price => 20)
 Product.create(:name => 'Advice3', :price => 30)
 
+Category.create(:categoryname => "Agriculture")
+Category.create(:categoryname => "Anthropology")
+Category.create(:categoryname => "Archaeology")
+Category.create(:categoryname => "Architecture and Design")
+Category.create(:categoryname => "Business")
+Category.create(:categoryname => "Chemistry")
+Category.create(:categoryname => "Computer Science")
+Category.create(:categoryname => "Construction")
+Category.create(:categoryname => "Creative Writing")
+Category.create(:categoryname => "Culinary Arts")
+Category.create(:categoryname => "Cultural Studies")
+Category.create(:categoryname => "Divinity")
+Category.create(:categoryname => "Earch Science")
+Category.create(:categoryname => "Economics")
+Category.create(:categoryname => "Education")
+Category.create(:categoryname => "Electrical")
+Category.create(:categoryname => "Engineering")
+Category.create(:categoryname => "Environmental Studies")
+Category.create(:categoryname => "Family Science")
+Category.create(:categoryname => "Film")
+Category.create(:categoryname => "Gender Studies")
+Category.create(:categoryname => "Geography")
+Category.create(:categoryname => "Health Science")
+Category.create(:categoryname => "History")
+Category.create(:categoryname => "Human Physical Performance")
+Category.create(:categoryname => "Journalism")
+Category.create(:categoryname => "Law")
+Category.create(:categoryname => "Library and Museum Studies")
+Category.create(:categoryname => "Life Science")
+Category.create(:categoryname => "Linguistics")
+Category.create(:categoryname => "Literature")
+Category.create(:categoryname => "Logic")
+Category.create(:categoryname => "Machinery")
+Category.create(:categoryname => "Mathematics")
+Category.create(:categoryname => "Mechanic")
+Category.create(:categoryname => "Music")
+Category.create(:categoryname => "Performing Arts")
+Category.create(:categoryname => "Philosophy")
+Category.create(:categoryname => "Photography")
+Category.create(:categoryname => "Physics")
+Category.create(:categoryname => "Plumbing")
+Category.create(:categoryname => "Political Science")
+Category.create(:categoryname => "Psychology")
+Category.create(:categoryname => "Public Administration")
+Category.create(:categoryname => "Religion")
+Category.create(:categoryname => "Social Work")
+Category.create(:categoryname => "Sociology")
+Category.create(:categoryname => "Space Science")
+Category.create(:categoryname => "Statistics")
+Category.create(:categoryname => "Systems Science")
+Category.create(:categoryname => "Transportation")
+Category.create(:categoryname => "Visual Arts")
+Category.create(:categoryname => "Welding")
+Category.create(:categoryname => "World Studies")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
