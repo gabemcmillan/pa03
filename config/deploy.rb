74221eb33d50b2b0e096cce5959db00e890f643d
gabem <gabemcmillan@gmail.com>
2013-05-23 21:09:17 -0500

    set use-sudo = true

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,35 +2,30 @@ require "bundler/capistrano"
 
 server "96.126.113.242", :web, :app, :db, primary: true
 
-set :user, 'deploy'
-set :domain, 'pathadvisor.org'
-set :application, "PathAdvisor"
- 
-set :repository, "https://github.com/gabemcmillan/#{application}.git"  # Your clone URL
-set :scm, "git"
-set :branch, "master"
-set :scm_verbose, true
-set :deploy_via, :remote_cache
-set :scm_passphrase, "copley66"  # The deploy user's password
+set :application, "pa03"
+set :user, "deploy"
 set :deploy_to, "/home/#{user}/rails32/#{application}"
-set :use_sudo, false
- 
+set :deploy_via, :remote_cache
+set :use_sudo, true
+
+set :scm, "git"
+set :repository, "git@github.com:gabemcmillan/#{application}.git"
+set :branch, "master"
+
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
- 
-role :web, domain                         # Your HTTP server, Apache/etc
-role :app, domain                          # This may be the same as your `Web` server
-role :db,  domain, :primary => true # This is where Rails migrations will run
- 
-namespace :deploy do
-  task :start do ; end
-  task :stop do ; end
-  task :restart, :roles => :app, :except => { :no_release => true } do
-    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-  end
-end
 
- task :setup_config, roles: :app do
+after "deploy", "deploy:cleanup" # keep only the last 5 releases
+
+namespace :deploy do
+  %w[start stop restart].each do |command|
+    desc "#{command} unicorn server"
+    task command, roles: :app, except: {no_release: true} do
+      run "/etc/init.d/unicorn_#{application} #{command}"
+    end
+  end
+
+  task :setup_config, roles: :app do
     sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
     sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
     run "mkdir -p #{shared_path}/config"
