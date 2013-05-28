85f5683c3d11cb88bdfd25974d4b4d985dbd2562
gabem <gabemcmillan@gmail.com>
2013-05-27 19:50:05 -0500

    ok

--- config/unicorn.rb
+++
@@ -1,9 +0,0 @@
-root = "/home/deploy/rails32/pa03/current"
-working_directory root
-pid "/home/deploy/rails32/pa03/shared/pids/unicorn.pid"
-stderr_path "/home/deploy/rails32/pa03/shared/log/unicorn.stderr.log"
-stdout_path "/home/deploy/rails32/pa03/shared/log/unicorn.stdout.log"
-
-listen "/tmp/unicorn.pa03.sock"
-worker_processes 2
-timeout 30
