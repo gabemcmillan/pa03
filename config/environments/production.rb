d52e3894f3da4a6959c5da9b0f5deabf44f544c1
gabem <gabemcmillan@gmail.com>
2013-06-01 21:55:28 -0500

    changed pa03 app to use pa02_production db

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,7 @@ Pa02::Application.configure do
 
   
   #specify what domain to use for mailer URLs
-  config.action_mailer.default_url_options = { :host => 'www.pathadvisor.com' }
+  config.action_mailer.default_url_options = { :host => 'www.pathadvisor.org' }
   
   #config.action_mailer.delivery_method = :letter_opener
   
